1310373376eb35761fdcd00f32f45960dc569acc
Kate Dubitski <kate.dubitski@gmail.com>
2020-05-07 17:15:19 -0400

    create and export remove function

--- src/helpers.js
+++ src/helpers.js
@@ -7,4 +7,15 @@
  const choice = (arr)=>{
    return arr[Math.floor(Math.random() * arr.length)];
  }
- export {choice};
+
+
+ const remove = (arr, item)=>{
+   for(let i = 0; i < arr.length; i++){
+     if(arr[i] === item){
+       return arr.splice(i,1);
+     }
+   }
+   return undefined;
+ }
+
+ export {choice,remove};
